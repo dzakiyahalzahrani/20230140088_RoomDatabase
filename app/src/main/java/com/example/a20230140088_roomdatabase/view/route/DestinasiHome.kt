75184d87230b5c62c8f47fbba116321e99f11c5c
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-11-27 13:54:37 +0700

    membuat DestinasiHome.kt

---
+++ app/src/main/java/com/example/a20230140088_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,11 @@
+package com.example.a20230140088_roomdatabase.view.route
+
+import com.example.a20230140088_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi{
+
+    override val route = "Home"
+
+    override val titleRes = R.string.app_name
+
+}
