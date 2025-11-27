45c4353caaff83dcc075c5a206ea95ea48ccdc2a
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-11-27 15:10:47 +0700

    memperbaiki SiswaDao.kt

--- app/src/main/java/com/example/a20230140088_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a20230140088_roomdatabase/room/SiswaDao.kt
@@ -2,6 +2,7 @@ package com.example.a20230140088_roomdatabase.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
@@ -10,6 +11,6 @@ interface SiswaDao {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
-    @Insert(onConflict = onConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 }
