96c0bb1c591afbe99b23a416f0384762f731fe9b
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-04 11:45:36 +0700

    menambahkan suspend delete dan override

--- app/src/main/java/com/example/a20230140088_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a20230140088_roomdatabase/repositori/RepositoriSiswa.kt
@@ -5,7 +5,9 @@ import com.example.a20230140088_roomdatabase.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
-    fun getAllSiswaStream(): Flow<List<Siswa>>
+    fun getAllSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
 
     suspend fun insertSiswa(siswa: Siswa)
 
@@ -18,4 +20,9 @@ class OfflineRepositoriSiswa(
         .getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao
         .insert(siswa)
+    override fun getSiswaStream(id: Int):Flow<Siswa?> = siswaDao
+        .getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao
+        .delete(siswa)
+
 }
