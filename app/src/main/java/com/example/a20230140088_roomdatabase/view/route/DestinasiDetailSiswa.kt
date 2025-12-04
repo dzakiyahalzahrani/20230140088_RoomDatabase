68adca3762a8dcf7b53a8b9c11ab479d3abb69a6
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-04 13:34:35 +0700

    membuat file DestinasiDetailSiswa

---
+++ app/src/main/java/com/example/a20230140088_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.a20230140088_roomdatabase.view.route
+
+import com.example.a20230140088_roomdatabase.R
+
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
